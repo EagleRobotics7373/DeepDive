903812c3ebe4555300c4b62acbd3a76ad6b9d2c4
7373-EagleRobotics <team7373robotics@gmail.com>
2024-12-03 17:14:11 -0500

    before meet3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneBlock.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneBlock.java
@@ -15,8 +15,8 @@ public class OneBlock extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-//        Servo bucket = hardwareMap.get(Servo.class, "Bucket");
-//        DcMotor arm = hardwareMap.get(DcMotor.class, "Arm");
+        DcMotor arm = hardwareMap.get(DcMotor.class, "Arm");
+        Servo bucket = hardwareMap.get(Servo.class, "Bucket");
 
         Pose2d startPose = new Pose2d(14, -56, Math.toRadians(0));
 
@@ -25,13 +25,35 @@ public class OneBlock extends LinearOpMode {
         waitForStart();
 
         TrajectorySequence trajSeqRIGHT = drive.trajectorySequenceBuilder(startPose)
+                .addTemporalMarker(1, ()-> {
+                    arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    bucket.setPosition(0);
+                })
                 .forward(22)
-                .strafeLeft(70)
-                .turn(Math.toRadians(135))
-                .lineToConstantHeading(new Vector2d(-53, -51))
-//                .addTemporalMarker(() -> arm.setMode(DcMotor.RunMode.RUN_TO_POSITION))
-//                .addTemporalMarker(() -> arm.setTargetPosition(12250))
-//                .addTemporalMarker(() -> arm.setPower(1))
+                .turn(Math.toRadians(90))
+                .forward(52)
+                .turn(Math.toRadians(45))
+                .forward(29)
+                .turn(Math.toRadians(10))
+                .addTemporalMarker(8, ()-> {
+                    arm.setPower(1);
+                    arm.setTargetPosition(12500);
+                    arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                })
+                .forward(2)
+                .addTemporalMarker(14, ()-> {
+                    bucket.setPosition(.3);
+                })
+                .addTemporalMarker(16, ()-> {
+                    arm.setPower(1);
+                    arm.setTargetPosition(0);
+                    arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                })
+                .waitSeconds(5)
+                .back(29)
+                .turn(Math.toRadians(-55))
+                .back(100)
+                .strafeLeft(50)
                 .build();
         drive.followTrajectorySequence(trajSeqRIGHT);
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMeet1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMeet1.java
@@ -1,16 +1,21 @@
 package org.firstinspires.ftc.teamcode;
 
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+
+
 
 
 @TeleOp
 public class TeleOpMeet1 extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+
 
         DcMotor rightFrontMotor = hardwareMap.get(DcMotor.class, "rightFront");
         DcMotor leftFrontMotor = hardwareMap.get(DcMotor.class, "leftFront");
@@ -23,26 +28,26 @@ public class TeleOpMeet1 extends LinearOpMode {
         rightRearMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
 
+
+
         int position = 0;
         float positionS = 0;
+        int up = 0;
         waitForStart();
+
 
         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+
         while (opModeIsActive()) {
-            if(arm.getCurrentPosition()>1000) {
-                bucket.setPosition(.8);
-            } else {
-                bucket.setPosition(.7);
-            }
-            if (gamepad1.dpad_up) {
-                position += 250;
-                sleep(200);
-            }
-            if (gamepad1.dpad_down) {
-                position -= 250;
-                sleep(200);
-            }
+//            if (gamepad2.dpad_up) {
+//                position += 250;
+//                sleep(100);
+//            }
+//            if (gamepad2.dpad_down) {
+//                position -= 250;
+//                sleep(100);
+//            }
             if (gamepad2.dpad_up) {
                 positionS += .1;
                 sleep(100);
@@ -51,34 +56,48 @@ public class TeleOpMeet1 extends LinearOpMode {
                 positionS -= .1;
                 sleep(100);
             }
-            if (gamepad2.y) {
-                arm.setTargetPosition(11750);
-                arm.setPower(1);
+            if(gamepad2.y) {
+                up = 1;
+                arm.setTargetPosition(12000);
+                arm.setPower(-1);
                 arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             }
-            if (gamepad2.a) {
+            if(gamepad2.a) {
+                up =0;
                 arm.setTargetPosition(0);
-                arm.setPower(1);
+                arm.setPower(-1);
                 arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             }
             if (gamepad2.left_trigger > .1) {
-                spin.setPower(.65);
+                spin.setPower(.6);
             }
             if (gamepad2.right_trigger > .1) {
                 spin.setPower(0);
             }
+            if (gamepad2.x) {
+                bucket.setPosition(.3);
+            }
+            if(arm.getCurrentPosition()>750 && arm.getCurrentPosition()<3500 && up ==0) {
+                bucket.setPosition(.3);
+            } else if(arm.getCurrentPosition() > 3500 && arm.getCurrentPosition()<4000) {
+                bucket.setPosition(.1);
+            } else if(arm.getCurrentPosition() <750) {
+                bucket.setPosition(.1);
+            }
             double y = gamepad1.left_stick_y;
-            double x = -gamepad1.left_stick_x * 1.1;
+            double x = gamepad1.left_stick_x * 1.1;
             double rx = -gamepad1.right_stick_x;
             y *= .65;
             x *= .65;
             rx *= .65;
 
-            if (gamepad1.left_trigger > 0) {
+
+            if(gamepad1.left_trigger > 0) {
                 y *= .5;
                 x *= .5;
                 rx *= .5;
             }
+
 
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             double frontLeftPower = (y + x + rx) / denominator;
@@ -86,14 +105,16 @@ public class TeleOpMeet1 extends LinearOpMode {
             double frontRightPower = (y - x - rx) / denominator;
             double backRightPower = (y + x - rx) / denominator;
 
+
             leftFrontMotor.setPower(frontLeftPower);
             leftRearMotor.setPower(backLeftPower);
             rightFrontMotor.setPower(frontRightPower);
             rightRearMotor.setPower(backRightPower);
 
-            telemetry.addData("Position Servo", positionS);
-            telemetry.addData("Position", position);
-            telemetry.addData("Position Arm", arm.getCurrentPosition());
+
+            telemetry.addData("Position Servo",bucket.getPosition());
+            telemetry.addData("Position",position);
+            telemetry.addData("Position Arm",arm.getCurrentPosition());
             telemetry.update();
         }
         rightFrontMotor.setPower(0.0);
@@ -101,4 +122,9 @@ public class TeleOpMeet1 extends LinearOpMode {
         rightRearMotor.setPower(0.0);
         leftRearMotor.setPower(0.0);
     }
-    }
+
+
+
+
+}
+
